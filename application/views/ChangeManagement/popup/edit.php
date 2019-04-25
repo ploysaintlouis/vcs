10d3ca0aa519005fea24d67a3426b5d8ef0d1af1
one <thanaprus.n@gmail.com>
2019-04-26 01:23:48 +0700

    fixed remake code popup edit change request

---
+++ application/views/ChangeManagement/popup/edit.php
@@ -0,0 +1,19 @@
+<div id="edit_input_modal" class="modal fade" role="dialog">
+		<div class="modal-dialog">
+			<div class="modal-content" style="border-radius:6px;">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h4 class="modal-title">Edit Change Input/Output of Functional Requirements ID: 
+                        <b> <?php echo $functionNo; ?> </b>
+                        <div>Project id <input type="text" value=<?php echo $projectId ?> /></div>
+					</h4>
+				</div>
+				<form method="post" id="changeInput_form" >
+					<div class="modal-body" id="input_detail" align="center">
+                    </div>
+                </form>
+		</div>
+	</div>
+<script>
+
+</script>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -33,4 +33,5 @@
 	<?php echo css_asset('_all-skins.min.css'); ?>
 
 	<?php echo css_asset('main.css'); ?>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
